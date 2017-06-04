43b4e741bd2bee2c9e8b4c8ea2408e1eaafede5d
hootnot <f.brekeveld@gmail.com>
2017-06-04 20:27:48 +0200

    changes to make setup do something more

--- setup.py
+++ setup.py
@@ -1,20 +1,63 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
 
-setup(
-    name='rejson',
-    version='0.1',
-    author='Redis Labs',
-    author_email='oss@redislabs.com',
-    description='ReJSON Python Client',
-    url='http://github.com/RedisLabs/rejson-py',
-    packages=find_packages(),
-    install_requires=['redis', 'hiredis'],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Database',
-    ]
-)
+from setuptools import setup, find_packages
+import os
+import sys
+import re
+import shutil
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def read_all(f):
+    with open(f) as I:
+        return I.read()
+
+requirements = map(str.strip, open("requirements.txt").readlines())
+
+version = get_version('rejson')
+
+if sys.argv[-1] == 'publish':
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    shutil.rmtree('dist')
+    shutil.rmtree('build')
+    shutil.rmtree('rejson.egg-info')
+    sys.exit()
+
+setup(name='rejson',
+      version=version,
+      description='ReJSON Python Client',
+      long_description=read_all("README.md"),
+      classifiers=[
+            'Programming Language :: Python',
+            'License :: OSI Approved :: BSD License',
+            'Intended Audience :: Developers',
+            'Operating System :: OS Independent',
+            'Development Status :: 4 - Beta',
+            'Topic :: Database',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.5',
+      ],  # Get from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='YAML parser configuration',
+      author='RedisLabs',
+      author_email='oss@redislabs.com',
+      url='http://github.com/RedisLabs/rejson-py',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      test_suite="tests",
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requirements,
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
